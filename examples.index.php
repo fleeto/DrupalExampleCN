d316e0a88a16a28e90b1f259f8eb1635d12a59de
Randy Fay <rfay@30906.no-reply.drupal.org>
2010-05-11 13:13:24 +0000

    #761334 (#3) by JohnAlbin: Create examples.index.php for doxy index.

---
+++ examples.index.php
@@ -0,0 +1,28 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Example modules
+ *
+ * - @link ajax_example.module How to use AJAX Forms @endlink
+ * - @link batch_example.module Using the Batch API @endlink
+ * - @link block_example.module How to define blocks @endlink
+ * - @link dbtng_example.module Examples of DBTNG database access in Drupal 7 @endlink
+ * - @link email_example.module How to send e-mail @endlink
+ * - @link field_example.module How to use fields in the new Field API @endlink
+ * - @link filter_example.module How to define a content filter @endlink
+ * - @link form_example.module Form API examples, including multistep forms @endlink
+ * - @link js_example.module Javascript examples for Drupal 7 @endlink
+ * - @link node_access_example.module Node access example @endlink
+ * - @link nodeapi_example.module Node API demonstrations showing how a separate module can change the behavior of a node @endlink
+ * - @link node_example.module How to define content (node) types in Drupal 7, including fields @endlink
+ * - @link node_access_example.module How to restrict access to nodes @endlink
+ * - @link nodeapi_example.module How to extend existing content types @endlink
+ * - @link page_example.module How to define pages @endlink
+ * - @link simpletest_example.module Demonstrates how to write tests for Drupal @endlink
+ * - @link token_example.module How to use tokens in Drupal 7 @endlink
+ * - @link trigger_example.module Implementing triggers and actions in Drupal 7 @endlink
+ * - @link vertical_tabs_example.module Using Vertical Tabs in Drupal 7 @endlink
+ * - @link xmlrpc_example.module XML-RPC Example @endlink
+ */
