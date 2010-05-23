f52e71cc1e750fd72f1ed400326ce90cf469bf1d
Randy Fay <rfay@30906.no-reply.drupal.org>
2010-05-23 06:00:15 +0000

    #765240 by coltrane, rfay: Add D7 queue example.

--- examples.index.php
+++ examples.index.php
@@ -5,24 +5,22 @@
  * @file
  * Example modules
  *
- * - @link ajax_example.module How to use AJAX Forms @endlink
- * - @link batch_example.module Using the Batch API @endlink
- * - @link block_example.module How to define blocks @endlink
- * - @link dbtng_example.module Examples of DBTNG database access in Drupal 7 @endlink
- * - @link email_example.module How to send e-mail @endlink
- * - @link field_example.module How to use fields in the new Field API @endlink
- * - @link filter_example.module How to define a content filter @endlink
+ * - @link ajax_example.module Using AJAX forms @endlink
+ * - @link batch_example.module Using the batch API @endlink
+ * - @link block_example.module Defining blocks @endlink
+ * - @link dbtng_example.module Database examples (DBTNG) @endlink
+ * - @link email_example.module Sending e-mail @endlink
+ * - @link field_example.module Defining fields in the field API @endlink
+ * - @link filter_example.module Defining an input filter @endlink
  * - @link form_example.module Form API examples, including multistep forms @endlink
- * - @link js_example.module Javascript examples for Drupal 7 @endlink
- * - @link node_access_example.module Node access example @endlink
+ * - @link js_example.module Javascript examples @endlink
  * - @link nodeapi_example.module Node API demonstrations showing how a separate module can change the behavior of a node @endlink
- * - @link node_example.module How to define content (node) types in Drupal 7, including fields @endlink
- * - @link node_access_example.module How to restrict access to nodes @endlink
- * - @link nodeapi_example.module How to extend existing content types @endlink
- * - @link page_example.module How to define pages @endlink
- * - @link simpletest_example.module Demonstrates how to write tests for Drupal @endlink
- * - @link token_example.module How to use tokens in Drupal 7 @endlink
- * - @link trigger_example.module Implementing triggers and actions in Drupal 7 @endlink
- * - @link vertical_tabs_example.module Using Vertical Tabs in Drupal 7 @endlink
- * - @link xmlrpc_example.module XML-RPC Example @endlink
+ * - @link node_example.module Creating custom node types, with fields @endlink
+ * - @link page_example.module Creating a custom page @endlink
+ * - @link queue_example.module Using the Queue API @endlink
+ * - @link simpletest_example.module Writing tests for Drupal @endlink
+ * - @link token_example.module Using tokens @endlink
+ * - @link trigger_example.module Implementing triggers and actions @endlink
+ * - @link vertical_tabs_example.module Using vertical tabs @endlink
+ * - @link xmlrpc_example.module XML-RPC example @endlink
  */
